f5dd3bc2069c2604ea6bbbd83ee7c9d606f28f1d
dominik-bauer <dominik_b@gmx.de>
2023-04-02 23:08:08 +0200

    correction and typing

--- logger.py
+++ logger.py
@@ -7,6 +7,6 @@ from datetime import datetime
 def logme(msg) -> None:
     dt_ger = datetime.now(pytz.timezone("Europe/Berlin"))
     tstamp = f"{dt_ger:'%F %T.%f'}"[:-3]
-    tmsg = f"{tstamp} | {msg}"
+    tmsg = f"{tstamp} - {msg}"
     print(tmsg)
-    send_message(os.environ[f"P2_ID"], os.environ[f"P2_PKEY"], tmsg, "")
+    send_message(os.environ["P2_ID"], os.environ["P2_PKEY"], msg, "")
